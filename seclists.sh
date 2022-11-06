da21b8a526d92afb2c77b52848a0bcc78a7fc85d
er072391 <76501872+eR072391@users.noreply.github.com>
2022-11-06 09:46:09 +0900

    Create seclists.sh

---
+++ seclists.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/danielmiessler/SecLists
+
+mv SecLists ~/SecLists; mkdir ~/SecLists/Often-Use-Directory
+
+cp ~/SecLists/Discovery/Web-Content/raft-large-directories-lowercase.txt ~/SecLists/Often-Use-Directory
+cp ~/SecLists/Discovery/Web-Content/raft-large-files-lowercase.txt ~/SecLists/Often-Use-Directory
+cp ~/SecLists/Discovery/Web-Content/burp-parameter-names.txt ~/SecLists/Often-Use-Directory
+cp ~/SecLists/Discovery/DNS/subdomains-top1million-110000.txt ~/SecLists/Often-Use-Directory
+cp ~/SecLists/Discovery/Passwords/rockyou.txt ~/SecLists/Often-Use-Directory
