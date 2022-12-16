355026c49dd000e34680724265f76f22bdbb136d
er072391 <76501872+eR072391@users.noreply.github.com>
2022-12-16 14:32:51 +0900

    Update tor-setup.sh

--- tor-setup.sh
+++ tor-setup.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-sudo apt install -y torbrowser-launcher proxychains
+sudo apt install -y torbrowser-launcher proxychains tor
 
 # Edit /etc/tor/torrc
 # ExcludeNodes {jp},{us},{gb},{ca},{au},{nz},{dk},{fr},{nl},{no},{de},{be},{it},{es},{il},{sg},{kr},{se},127.0.0.1
