b9f45725a44d14a76f3fb2c370033ed0bb71944c
er072391 <76501872+eR072391@users.noreply.github.com>
2023-01-06 14:19:28 +0900

    Update seclists.sh

--- seclists.sh
+++ seclists.sh
@@ -8,4 +8,4 @@ cp ~/SecLists/Discovery/Web-Content/raft-large-directories-lowercase.txt ~/SecLi
 cp ~/SecLists/Discovery/Web-Content/raft-large-files-lowercase.txt ~/SecLists/Often-Use-Directory
 cp ~/SecLists/Discovery/Web-Content/burp-parameter-names.txt ~/SecLists/Often-Use-Directory
 cp ~/SecLists/Discovery/DNS/subdomains-top1million-110000.txt ~/SecLists/Often-Use-Directory
-cp ~/SecLists/Discovery/Passwords/rockyou.txt ~/SecLists/Often-Use-Directory
+cp ~/SecLists/Passwords/Leaked-Databases/rockyou.txt.tar.gz ~/SecLists/Often-Use-Directory
