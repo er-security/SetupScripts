4a327dc493913764175dd90a00e35b6890647a12
er072391 <76501872+eR072391@users.noreply.github.com>
2023-02-25 22:36:49 +0900

    Update ffuf.sh

--- ffuf.sh
+++ ffuf.sh
@@ -3,6 +3,11 @@
 sudo apt install golang -y
 
 wget https://github.com/ffuf/ffuf/releases/download/v1.5.0/ffuf_1.5.0_linux_amd64.tar.gz
-tar -zxvf ffuf_1.5.0_linux_amd64.tar.gz
+mkdir ffuf
+tar -zxvf ffuf_1.5.0_linux_amd64.tar.gz -C ./ffuf
+cd ffuf
 
 sudo cp ffuf /usr/local/bin/
+rm *
+cd ..
+rm -rf ffuf
