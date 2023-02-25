131886934bc9717e05f6a23b5b84669fefcc24e9
er072391 <76501872+eR072391@users.noreply.github.com>
2023-02-25 22:38:42 +0900

    Update seclists.sh

--- seclists.sh
+++ seclists.sh
@@ -2,10 +2,10 @@
 
 git clone https://github.com/danielmiessler/SecLists
 
-mv SecLists ~/SecLists; mkdir ~/SecLists/Often-Use-Directory
+mv SecLists ~/SecLists; mkdir ~/SecLists/favorite-wordlists
 
-cp ~/SecLists/Discovery/Web-Content/raft-large-directories-lowercase.txt ~/SecLists/Often-Use-Directory
-cp ~/SecLists/Discovery/Web-Content/raft-large-files-lowercase.txt ~/SecLists/Often-Use-Directory
-cp ~/SecLists/Discovery/Web-Content/burp-parameter-names.txt ~/SecLists/Often-Use-Directory
-cp ~/SecLists/Discovery/DNS/subdomains-top1million-110000.txt ~/SecLists/Often-Use-Directory
-cp ~/SecLists/Passwords/Leaked-Databases/rockyou.txt.tar.gz ~/SecLists/Often-Use-Directory
+cp ~/SecLists/Discovery/Web-Content/raft-large-directories-lowercase.txt ~/SecLists/favorite-wordlists
+cp ~/SecLists/Discovery/Web-Content/raft-large-files-lowercase.txt ~/SecLists/favorite-wordlists
+cp ~/SecLists/Discovery/Web-Content/burp-parameter-names.txt ~/SecLists/favorite-wordlists
+cp ~/SecLists/Discovery/DNS/subdomains-top1million-110000.txt ~/SecLists/favorite-wordlists
+cp ~/SecLists/Passwords/Leaked-Databases/rockyou.txt.tar.gz ~/SecLists/favorite-wordlists
